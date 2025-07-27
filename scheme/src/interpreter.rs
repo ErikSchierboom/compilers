dfcda1f850e5947c85fe23125dedb9074da87332
Erik Schierboom <erik_schierboom@hotmail.com>
2025-07-27 15:20:04 +0200

    Create interpreter structure

--- scheme/src/interpreter.rs
+++ scheme/src/interpreter.rs
@@ -1,13 +1,35 @@
-use crate::parser::parse;
-use crate::scanner::{ScanError, Token};
+use crate::interpreter;
+use crate::parser::{parse, Node, ParseError};
+use crate::scanner::Token;
 
 #[derive(Debug, Clone)]
-pub enum ParseError {
-    ScanError(ScanError),
-    ExpectedToken(Token),
-    UnexpectedToken(Token)
+pub enum InterpretError {
+    ParseError(ParseError),
+    OperatorIsNotProcedure(Token)
+}
+struct Interpreter {
+    nodes: Vec<Node>,
+    errors: Vec<InterpretError>
 }
 
-pub fn interpret<'a>(source_code: &'a str) {
+impl Interpreter {
+    fn new(nodes: Vec<Node>, parse_errors: Vec<ParseError>) -> Self {
+        let errors = parse_errors
+            .into_iter()
+            .map(InterpretError::ParseError)
+            .collect();
+
+        Self { nodes, errors }
+    }
+
+    pub fn interpret(&mut self) -> (Vec<i32>, Vec<InterpretError>) {
+        panic!("TODO")
+    }
+}
+
+pub fn interpret(source_code: &str) -> (Vec<i32>, Vec<InterpretError>) {
     let (nodes, errors) = parse(source_code);
+
+    let mut interpreter = Interpreter::new(nodes, errors);
+    interpreter.interpret()
 }
