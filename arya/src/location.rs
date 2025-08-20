ae84be97617a4e0e100a1c5f34e42c59ba029095
Erik Schierboom <erik_schierboom@hotmail.com>
2025-08-20 11:25:35 +0200

    Add tests for span

--- arya/src/location.rs
+++ arya/src/location.rs
@@ -15,6 +15,28 @@ impl Span {
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_merge() {
+        let lhs = Span::new(4, 3);
+        let rhs = Span::new(9, 1);
+        let result = lhs.merge(&rhs);
+        assert_eq!(result.position, 4);
+        assert_eq!(result.length, 6);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_bad_merge() {
+        let lhs = Span::new(2, 3);
+        let rhs = Span::new(0, 2);
+        lhs.merge(&rhs);
+    }
+}
+
 #[derive(Clone, Debug)]
 pub struct Spanned<T> {
     pub value: T,
