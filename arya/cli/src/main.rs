cf6349255ebad5ce255db066855416b80aae08a6
Erik Schierboom <erik_schierboom@hotmail.com>
2025-09-11 20:28:49 +0200

    Easier way to test

--- arya/cli/src/main.rs
+++ arya/cli/src/main.rs
@@ -1,34 +1,58 @@
+use compiler::lexer::tokenize;
 use compiler::location::LineEndings;
 use compiler::parser::parse;
 
 fn main() {
-    let source = " 1 [1 8 3] [1 2 3 ; 4 5 6]";
-    let line_endings = LineEndings::new(source);
+    let source = "1 [1 8 3] [1 2 3 ; 4 5 6]";
 
-    for result in parse(source) {
+    test_tokenize(source)
+    // test_parse(source)
+    // test_interpret(source)
+}
+
+fn test_tokenize(source: &str) {
+    let line_endings = LineEndings::new(source);
+    for result in tokenize(source) {
         match result {
-            Ok(word) => {
-                let location = line_endings.location(&word.span);
-                eprintln!("Word at {}:\n{}", location, word.value)
+            Ok(spanned_token) => {
+                let location = line_endings.location(&spanned_token.span);
+                eprintln!("Token at {}: {}", location, spanned_token.value)
             }
-            Err(error) => {
-                let location = line_endings.location(&error.span);
-                eprintln!("Error at {}:\n{}", location, error.value)
+            Err(spanned_error) => {
+                let location = line_endings.location(&spanned_error.span);
+                eprintln!("Lex error at {}: {}", location, spanned_error.value)
             }
         }
     }
-
-
-    // match interpret(&source) {
-    //     Ok(mut values) => {
-    //         while let Some(value) = values.pop() {
-    //             println!("{:}", value)
-    //         }
-    //     }
-    //     Err(error) => {
-    //         let line_endings = LineEndings::new(source);
-    //         let location = line_endings.location(&error.span);
-    //         eprintln!("Error at {}: {}", location, error.value)
-    //     }
-    // }
 }
+
+fn test_parse(source: &str) {
+    let line_endings = LineEndings::new(source);
+    for result in parse(source) {
+        match result {
+            Ok(spanned_word) => {
+                let location = line_endings.location(&spanned_word.span);
+                eprintln!("Word at {}:\n{}", location, spanned_word.value)
+            }
+            Err(spanned_error) => {
+                let location = line_endings.location(&spanned_error.span);
+                eprintln!("Parse error at {}:\n{}", location, spanned_error.value)
+            }
+        }
+    }
+}
+
+// fn test_interpret(source: &str) {
+//     match interpret(&source) {
+//         Ok(mut values) => {
+//             while let Some(value) = values.pop() {
+//                 println!("{:}", value)
+//             }
+//         }
+//         Err(error) => {
+//             let line_endings = LineEndings::new(source);
+//             let location = line_endings.location(&error.span);
+//             eprintln!("Error at {}: {}", location, error.value)
+//         }
+//     }
+// }
