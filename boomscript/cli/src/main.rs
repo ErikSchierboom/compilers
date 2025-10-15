ff561b99ffa6259e43e75331050793b13f3fa996
Erik Schierboom <erik_schierboom@hotmail.com>
2025-10-15 13:04:32 +0200

    Change main

--- boomscript/cli/src/main.rs
+++ boomscript/cli/src/main.rs
@@ -2,7 +2,7 @@ use compiler::interpreter::interpret;
 
 fn main() {
     const SOURCE: &str = r#"
-        -1 [[6 4 5] [7 8 9]] + ?
+        [[1]]
     "#;
 
     println!("{:?}", interpret(SOURCE))
