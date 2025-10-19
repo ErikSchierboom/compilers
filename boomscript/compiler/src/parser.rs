c50a1dd2422437d08b6148c7eca2d3ccd26a4cf6
Erik Schierboom <erik_schierboom@hotmail.com>
2025-10-19 12:19:29 +0200

    Add tests for lambdas

--- boomscript/compiler/src/parser.rs
+++ boomscript/compiler/src/parser.rs
@@ -488,6 +488,16 @@ mod tests {
     }
 
     #[test]
+    fn test_parse_lambdas() {
+        let mut words = parse("() (1) (2 swap dup)");
+
+        assert_eq!(Some(Ok(Spanned::new(Word::Lambda(vec![]), Span::new(0, 2)))), words.next());
+        assert_eq!(Some(Ok(Spanned::new(Word::Lambda(vec![Spanned::new(Word::Integer(1), Span::new(4, 1))]), Span::new(3, 3)))), words.next());
+        assert_eq!(Some(Ok(Spanned::new(Word::Lambda(vec![Spanned::new(Word::Integer(2), Span::new(8, 1)), Spanned::new(Word::Identifier("swap".to_string()), Span::new(10, 4)), Spanned::new(Word::Identifier("dup".to_string()), Span::new(15, 3))]), Span::new(7, 12)))), words.next());;
+        assert_eq!(None, words.next())
+    }
+
+    #[test]
     fn test_parse_ignores_whitespace() {
         let mut words = parse(" \r\n\t \n");
 
