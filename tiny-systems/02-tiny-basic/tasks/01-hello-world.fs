d93dd9bcfd9465ce24315101edad58859e63798d
Erik Schierboom <erik_schierboom@hotmail.com>
2025-12-15 10:13:17 +0100

    tiny-basic/01: solve

--- tiny-systems/02-tiny-basic/tasks/01-hello-world.fs
+++ tiny-systems/02-tiny-basic/tasks/01-hello-world.fs
@@ -2,12 +2,6 @@
 // 01 - Add GOTO and better PRINT for infinite loop fun!
 // ----------------------------------------------------------------------------
 
-// NOTE: You can run this using 'dotnet run' from the terminal. 
-// If you want to run code in a different file, you will need to change
-// the 'tinybasic.fsproj' file (which references this source file now).
-
-// NOTE: F# code in projects is generally organized using namespaces and modules.
-// Here, we declare module name for the source code in this file.
 module TinyBASIC
 
 type Value =
@@ -32,41 +26,39 @@ type State =
 // ----------------------------------------------------------------------------
 
 let printValue value = 
-  // TODO: Take 'value' of type 'Value', pattern match on it and print it nicely.
-  failwith "not implemented"
+  match value with
+  | StringValue s -> printfn $"%s{s}"
 
 let getLine state line =
-  // TODO: Get a line with a given number from 'state.Program' (this can fail 
-  // if the line is not there.) You need this in the 'Goto' command case below.
-  failwith "not implemented"
+  match state.Program |> List.tryFind (fun (n, _) -> n = line) with
+  | Some (_, cmd) -> cmd
+  | None -> failwith "line not found"
 
 // ----------------------------------------------------------------------------
 // Evaluator
 // ----------------------------------------------------------------------------
 
-let rec evalExpression expr = 
-  // TODO: Implement evaluation of expressions. The function should take 
-  // 'Expression' and return 'Value'. In this step, it is trivial :-)
-  failwith "not implemented"
+let rec evalExpression expr =
+  match expr with
+  | Const value -> value
 
 let rec runCommand state (line, cmd) =
   match cmd with 
   | Print(expr) ->
-      // TODO: Evaluate the expression and print the resulting value here!
-      failwith "not implemented"
+      let value = evalExpression expr
+      printValue value
       runNextLine state line
   | Run ->
       let first = List.head state.Program    
       runCommand state first
   | Goto(line) ->
-      // TODO: Find the right line of the program using 'getLine' and call 
-      // 'runCommand' recursively on the found line to evaluate it.
-      failwith "not implemented"
+      let cmd = getLine state line
+      runCommand state (line, cmd)
 
 and runNextLine state line = 
-  // TODO: Find a program line with the number greater than 'line' and evalaute
-  // it using 'runCommand' (if found) or just return 'state' (if not found).
-  failwith "not implemented"
+  match state.Program |> List.tryFind (fun (n, _) -> n > line) with
+  | Some (next_line, next_cmd) -> runCommand state (next_line, next_cmd)
+  | None -> state
 
 // ----------------------------------------------------------------------------
 // Test cases
