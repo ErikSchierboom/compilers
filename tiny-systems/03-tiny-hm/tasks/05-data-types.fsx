cc2bd9f0c339e537ca0bb9bfce09a9c2b24ec949
Erik Schierboom <erik_schierboom@hotmail.com>
2025-12-16 13:23:50 +0100

    tiny-ml/05: solve

--- tiny-systems/03-tiny-hm/tasks/05-data-types.fsx
+++ tiny-systems/03-tiny-hm/tasks/05-data-types.fsx
@@ -10,7 +10,6 @@ type Expression =
   | Application of Expression * Expression
   | Lambda of string * Expression
   | Let of string * Expression * Expression
-  // NOTE: Added two types of expression for working with tuples
   | Tuple of Expression * Expression
   | TupleGet of bool * Expression
 
@@ -20,27 +19,51 @@ type Type =
   | TyNumber 
   | TyList of Type
   | TyFunction of Type * Type
-  // NOTE: Added type for tuples
   | TyTuple of Type * Type
 
 // ----------------------------------------------------------------------------
 // Constraint solving
 // ----------------------------------------------------------------------------
 
-let rec occursCheck vcheck ty = 
-  // TODO: Add case for 'TyTuple' (same as 'TyFunction')
-  failwith "not implemented"
+let rec occursCheck vcheck ty =
+  match ty with
+  | TyVariable v -> v = vcheck
+  | TyBool -> false
+  | TyNumber -> false
+  | TyList t -> occursCheck vcheck t
+  | TyFunction(ta, tb) -> occursCheck vcheck ta || occursCheck vcheck tb
+  | TyTuple(ta, tb) -> occursCheck vcheck ta || occursCheck vcheck tb
 
-let rec substType (subst:Map<_, _>) t1 = 
-  // TODO: Add case for 'TyTuple' (same as 'TyFunction')
-  failwith "not implemented"
+let rec substType (subst:Map<string, Type>) ty = 
+  match ty with
+  | TyVariable v -> subst |> Map.tryFind v |> Option.defaultValue ty
+  | TyBool -> ty
+  | TyNumber -> ty
+  | TyList t -> TyList (substType subst t)
+  | TyFunction(ta, tb) -> TyFunction(substType subst ta, substType subst tb)
+  | TyTuple(ta, tb) -> TyTuple(substType subst ta, substType subst tb)
 
-let substConstrs subst cs = 
-  failwith "implemented in step 2"
+let substConstrs (subst:Map<string, Type>) (cs:list<Type * Type>) =
+  cs |> List.map (fun (t1, t2) -> (substType subst t1, substType subst t2))
  
 let rec solve constraints =
-  // TODO: Add case for 'TyTuple' (same as 'TyFunction')
-  failwith "not implemented"
+  match constraints with 
+  | [] -> []
+  | (TyNumber, TyNumber)::cs -> solve cs
+  | (TyBool, TyBool)::cs -> solve cs
+  | (TyList t1, TyList t2)::cs -> solve ((t1, t2)::cs)
+  | (n, TyVariable v)::constraints 
+  | (TyVariable v, n)::constraints ->
+      if occursCheck v n then failwith "Cannot be solved (occurs check)"
+      let constraints = substConstrs (Map.ofList [(v, n)]) constraints
+      let subst = solve constraints
+      let n = substType (subst |> Map.ofList) n 
+      (v, n)::subst
+    | (TyFunction(ta1, tb1), TyFunction(ta2, tb2)):: constraints ->
+      solve ((ta1, ta2)::(tb1, tb2)::constraints)
+    | (TyTuple(ta1, tb1), TyTuple(ta2, tb2)):: constraints ->
+      solve ((ta1, ta2)::(tb1, tb2)::constraints)
+    | _ -> failwith "Cannot be solved (type mismatch)"
 
 
 // ----------------------------------------------------------------------------
@@ -55,27 +78,65 @@ let newTyVariable =
 
 let rec generate (ctx:TypingContext) e = 
   match e with 
-  | Constant _ -> failwith "implemented in step 3"
-  | Binary("+", e1, e2) -> failwith "implemented in step 3"
-  | Binary("=", e1, e2) -> failwith "implemented in step 3"
-  | Binary(op, _, _) -> failwith "implemented in step 3"
-  | Variable v -> failwith "implemented in step 3"
-  | If(econd, etrue, efalse) -> failwith "implemented in step 3"
-  | Let(v, e1, e2) -> failwith "implemented in step 4"
-  | Lambda(v, e) -> failwith "implemented in step 4"
-  | Application(e1, e2) -> failwith "implemented in step 4"
+  | Constant _ ->
+      TyNumber, []
+
+  | Binary("+", e1, e2)
+  | Binary("*", e1, e2) ->
+      let t1, s1 = generate ctx e1
+      let t2, s2 = generate ctx e2
+      TyNumber, s1 @ s2 @ [ t1, TyNumber; t2, TyNumber ]      
+
+  | Binary("=", e1, e2) ->
+      let t1, s1 = generate ctx e1
+      let t2, s2 = generate ctx e2
+      TyBool, s1 @ s2 @ [ t1, TyNumber; t2, TyNumber ]
+
+  | Binary(op, _, _) ->
+      failwithf "Binary operator '%s' not supported." op
+
+  | Variable v ->
+      ctx[v], []
+
+  | If(econd, etrue, efalse) ->
+      let t1, s1 = generate ctx econd
+      let t2, s2 = generate ctx etrue
+      let t3, s3 = generate ctx efalse
+      
+      TyBool, s1 @ s2 @ s3 @ [ t1, TyBool; t2, t3 ]
+
+  | Let(v, e1, e2) ->
+      let t1, s1 = generate ctx e1
+      let t2, s2 = generate (Map.add v t1 ctx) e2
+      t2, s1 @ s2
+  
+  | Lambda(v, e) ->
+      let targ = newTyVariable()
+      let t1, s1 = generate (Map.add v targ ctx) e
+      TyFunction(targ, t1), s1
+
+  | Application(e1, e2) ->
+      let targ = newTyVariable()
+      let t1, s1 = generate ctx e1
+      let t2, s2 = generate ctx e2
+      targ, s1 @ s2 @ [ t1, TyFunction(t2, targ) ]
 
   | Tuple(e1, e2) ->
-      // TODO: Easy. The returned type is composed of the types of 'e1' and 'e2'.
-      failwith "not implemented"
+      let t1, s1 = generate ctx e1
+      let t2, s2 = generate ctx e2
+      TyTuple(t1, t2), s1 @ s2
 
   | TupleGet(b, e) ->
-      // TODO: Trickier. The type of 'e' is some tuple, but we do not know what.
-      // We need to generate two new type variables and a constraint.
-      failwith "not implemented"
+      let targ1 = newTyVariable()
+      let targ2 = newTyVariable()
+
+      let t, s = generate ctx e
+      if b then
+        targ1, s @ [ t, TyTuple(targ1, targ2) ]
+      else
+        targ2, s @ [ t, TyTuple(targ1, targ2) ]
 
   
-
 // ----------------------------------------------------------------------------
 // Putting it together & test cases
 // ----------------------------------------------------------------------------
