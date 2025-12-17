a41c273c92f91d3d28f9d11c81ad258ea6a4c8b0
Erik Schierboom <erik_schierboom@hotmail.com>
2025-12-17 10:33:38 +0100

    tiny-prolog/01: solve

--- tiny-systems/04-tiny-prolog/tasks/01-basic-unification.fsx
+++ tiny-systems/04-tiny-prolog/tasks/01-basic-unification.fsx
@@ -33,26 +33,21 @@ let rule p b = { Head = p; Body = b }
 let rec unifyLists l1 l2 : option<list<string * Term>> = 
   match l1, l2 with 
   | [], [] -> 
-      // TODO: Succeeds, but returns an empty substitution
-      failwith "not implemented"
-  | h1::t1, h2::t2 -> 
-      // TODO: Unify 'h1' with 'h2' using 'unify' and
-      // 't1' with 't2' using 'unifyLists'. If both 
-      // succeed, return the generated joint substitution!
-      failwith "not implemented"
+      Some []
+  | h1::t1, h2::t2 ->
+      match unify h1 h2, unifyLists t1 t2 with
+      | Some s1, Some s2 -> Some (s1 @ s2)      
+      | _ ->  None
   | _ -> 
-    // TODO: Lists cannot be unified 
-    failwith "not implemented"
+    None
 
 and unify t1 t2 : option<list<string * Term>> = 
   match t1, t2 with 
-  | _ ->
-      // TODO: Add all the necessary cases here!
-      // * For matching atoms, return empty substitution
-      // * For matching predicates, return the result of 'unifyLists'
-      // * For variable and any term, return a new substitution
-      // * For anything else, return None (failed to unify) 
-      failwith "not implemented"
+  | Atom a1, Atom a2 when a1 = a2 -> Some []
+  | Predicate (p1, t1), Predicate (p2, t2) when p1 = p2 -> unifyLists t1 t2
+  | Variable v, term
+  | term, Variable v -> Some [(v, term)]
+  | _ -> None
 
 // ----------------------------------------------------------------------------
 // Basic unification tests 
