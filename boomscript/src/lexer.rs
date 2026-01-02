915d7827f84e5bffc12151a988ffe403e488f45f
Erik Schierboom <erik_schierboom@hotmail.com>
2026-01-02 08:39:00 +0100

    Use spanned in lexer

--- boomscript/src/lexer.rs
+++ boomscript/src/lexer.rs
@@ -1,55 +1,28 @@
-use crate::location::Span;
+use crate::location::Spanned;
 use std::iter::{Enumerate, Peekable};
 
 #[derive(Debug)]
 pub enum LexError {
-    ExpectedCharacter(Span),
-    UnexpectedCharacter(char, Span),
-    InvalidEscape(char, Span),
-}
-
-impl LexError {
-    pub fn location(&self) -> &Span {
-        match self {
-            LexError::ExpectedCharacter(location) |
-            LexError::UnexpectedCharacter(_, location) |
-            LexError::InvalidEscape(_, location) => location
-        }
-    }
+    ExpectedCharacter,
+    UnexpectedCharacter(char),
+    InvalidEscape(char),
 }
 
 #[derive(Clone, Debug)]
 pub enum Token {
     // Literals
-    Int(Span),
-    Float(Span),
-    Char(Span),
-    String(Span),
-    Quote(Span),
-    Word(Span),
+    Int,
+    Float,
+    Char,
+    String,
+    Quote,
+    Word,
 
     // Delimiters
-    OpenBracket(Span),
-    CloseBracket(Span),
-    OpenParen(Span),
-    CloseParen(Span),
-}
-
-impl Token {
-    pub fn location(&self) -> &Span {
-        match self {
-            Token::Int(location) |
-            Token::Float(location) |
-            Token::Char(location) |
-            Token::String(location) |
-            Token::Quote(location) |
-            Token::Word(location) |
-            Token::OpenBracket(location) |
-            Token::CloseBracket(location) |
-            Token::OpenParen(location) |
-            Token::CloseParen(location) => location
-        }
-    }
+    OpenBracket,
+    CloseBracket,
+    OpenParen,
+    CloseParen,
 }
 
 struct Lexer<T: Iterator<Item=char>> {
@@ -62,29 +35,29 @@ impl<T: Iterator<Item=char>> Lexer<T> {
     }
 
     // TODO: lex whitespace
-    fn tokenize(&mut self) -> Result<Vec<Token>, Vec<LexError>> {
+    fn tokenize(&mut self) -> Result<Vec<Spanned<Token>>, Vec<Spanned<LexError>>> {
         let mut tokens = Vec::new();
         let mut errors = Vec::new();
 
         while let Some((start, c, end)) = self.advance() {
             match c {
                 ' ' | '\t' | '\r' | '\n' => continue,
-                '[' => tokens.push(Token::OpenBracket(Span { start, end })),
-                ']' => tokens.push(Token::CloseBracket(Span { start, end })),
-                '(' => tokens.push(Token::OpenParen(Span { start, end })),
-                ')' => tokens.push(Token::CloseParen(Span { start, end })),
+                '[' => tokens.push(Spanned::new(Token::OpenBracket, start, end)),
+                ']' => tokens.push(Spanned::new(Token::CloseBracket, start, end)),
+                '(' => tokens.push(Spanned::new(Token::OpenParen, start, end)),
+                ')' => tokens.push(Spanned::new(Token::CloseParen, start, end)),
                 '\'' => {
                     let end = self.advance_while(Self::is_word_character, start);
-                    tokens.push(Token::Quote(Span { start, end }))
+                    tokens.push(Spanned::new(Token::Quote, start, end))
                 }
                 '0'..='9' => {
                     let end = self.advance_while(char::is_ascii_digit, start);
 
                     if self.advance_if_eq(&'.') {
                         let end = self.advance_while(char::is_ascii_digit, end + 1);
-                        tokens.push(Token::Float(Span { start, end }))
+                        tokens.push(Spanned::new(Token::Float, start, end))
                     } else {
-                        tokens.push(Token::Int(Span { start, end }))
+                        tokens.push(Spanned::new(Token::Int, start, end))
                     }
                 }
                 '#' => {
@@ -94,37 +67,37 @@ impl<T: Iterator<Item=char>> Lexer<T> {
                                 Some((_, 'n', end)) |
                                 Some((_, 'r', end)) |
                                 Some((_, 't', end)) |
-                                Some((_, '\'', end)) => tokens.push(Token::Char(Span { start, end })),
-                                Some((start, c, end)) => errors.push(LexError::InvalidEscape(c, Span { start, end })),
-                                None => errors.push(LexError::ExpectedCharacter(Span { start, end })),
+                                Some((_, '\'', end)) => tokens.push(Spanned::new(Token::Char, start, end)),
+                                Some((start, c, end)) => errors.push(Spanned::new(LexError::InvalidEscape(c), start, end)),
+                                None => errors.push(Spanned::new(LexError::ExpectedCharacter, start, end)),
                             }
                         }
-                        Some((start, _, end)) => tokens.push(Token::Char(Span { start, end })),
-                        None => errors.push(LexError::ExpectedCharacter(Span { start: start + 1, end: end + 1 }))
+                        Some((start, _, end)) => tokens.push(Spanned::new(Token::Char, start, end)),
+                        None => errors.push(Spanned::new(LexError::ExpectedCharacter, start + 1, end + 1))
                     }
                 }
                 '"' => {
                     loop {
                         match self.advance() {
-                            Some((start, '"', end)) => tokens.push(Token::String(Span { start, end })),
+                            Some((start, '"', end)) => tokens.push(Spanned::new(Token::String, start, end)),
                             Some((start, '\\', end)) => match self.advance() {
                                 Some((_, 'n', _)) |
                                 Some((_, 'r', _)) |
                                 Some((_, 't', _)) |
                                 Some((_, '"', _)) => {}
-                                Some((start, c, end)) => errors.push(LexError::InvalidEscape(c, Span { start, end })),
-                                None => errors.push(LexError::ExpectedCharacter(Span { start, end })),
+                                Some((start, c, end)) => errors.push(Spanned::new(LexError::InvalidEscape(c), start, end)),
+                                None => errors.push(Spanned::new(LexError::ExpectedCharacter, start, end)),
                             },
                             Some(_) |
-                            None => errors.push(LexError::ExpectedCharacter(Span { start: start + 1, end: end + 1 })),
+                            None => errors.push(Spanned::new(LexError::ExpectedCharacter, start + 1, end + 1)),
                         }
                     }
                 }
                 c if Self::is_word_character(&c) => {
                     let end = self.advance_while(Self::is_word_character, start);
-                    tokens.push(Token::Word(Span { start, end }))
+                    tokens.push(Spanned::new(Token::Word, start, end))
                 }
-                _ => errors.push(LexError::UnexpectedCharacter(c, Span { start, end })),
+                _ => errors.push(Spanned::new(LexError::UnexpectedCharacter(c), start, end))
             }
         }
 
@@ -152,7 +125,7 @@ impl<T: Iterator<Item=char>> Lexer<T> {
     }
 }
 
-pub fn tokenize(code: &str) -> Result<Vec<Token>, Vec<LexError>> {
+pub fn tokenize(code: &str) -> Result<Vec<Spanned<Token>>, Vec<Spanned<LexError>>> {
     let mut lexer = Lexer::new(code.chars());
     lexer.tokenize()
 }
