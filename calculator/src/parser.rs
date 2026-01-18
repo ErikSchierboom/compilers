15ec26195af37efc1baabd96b260016d60da6578
Erik Schierboom <erik_schierboom@hotmail.com>
2026-01-18 10:06:22 +0100

    Add calculator

---
+++ calculator/src/parser.rs
@@ -0,0 +1,68 @@
+use std::iter::Peekable;
+use crate::lexer::{tokenize, LexicalError, Token};
+use crate::recursive_descent_parser::RecursiveDescentParser;
+
+#[derive(Debug)]
+pub enum ParseError {
+    Lexical(LexicalError),
+    UnexpectedEndOfFile,
+    ExpectedToken(Token),
+    UnexpectedToken(Token),
+}
+
+pub enum Expression {
+    Number(i64),
+    Unary(Box<Expression>, UnaryOperator),
+    Binary(Box<Expression>, BinaryOperator, Box<Expression>),
+    Grouping(Box<Expression>)
+}
+
+#[derive(Debug)]
+pub enum BinaryOperator {
+    Add,
+    Sub,
+    Mul,
+    Div
+}
+
+#[derive(Debug)]
+pub enum UnaryOperator {
+    Pos,
+    Neg,
+}
+
+impl From<Token> for UnaryOperator {
+    fn from(value: Token) -> Self {
+        match value {
+            Token::Plus => Self::Pos,
+            Token::Minus => Self::Neg,
+            _ => panic!("cannot convert token to unary operator")
+        }
+    }
+}
+
+impl From<Token> for BinaryOperator {
+    fn from(value: Token) -> Self {
+        match value {
+            Token::Plus => Self::Add,
+            Token::Minus => Self::Sub,
+            Token::Star => Self::Mul,
+            Token::Slash => Self::Div,
+            _ => panic!("cannot convert token to binary operator")
+        }
+    }
+}
+
+// TODO: add trait
+// TODO: implement recursive descent parser
+// TODO: implement Shunting Yard parser
+// TODO: implement Pratt parser
+
+
+
+pub fn parse(code: &str) -> Result<Expression, ParseError> {
+    match tokenize(code) {
+        Ok(tokens) => RecursiveDescentParser::new(tokens.into_iter()).parse(),
+        Err(error) => Err(ParseError::Lexical(error)),
+    }
+}
