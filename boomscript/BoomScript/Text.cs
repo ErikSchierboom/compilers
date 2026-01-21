9bcce4fed40c75b65e5def5fbc070fa21212611b
Erik Schierboom <erik_schierboom@hotmail.com>
2026-01-21 12:45:35 +0100

    Simplify line start searching

--- boomscript/BoomScript/Text.cs
+++ boomscript/BoomScript/Text.cs
@@ -11,32 +11,55 @@ public sealed record SourceText(string Text)
 
 public sealed class TextLines
 {
-    private readonly int[] _lineStarts;
+    private readonly int[] _lines;
     
     public TextLines(SourceText text)
     {
-        var lineStarts = new List<int> { 0 };
+        var lines = new List<int> { 0 };
 
         for (var i = 0; i < text.Text.Length; i++)
         {
             if (text.Text[i] == '\n')
-                lineStarts.Add(i + 1);
+                lines.Add(i + 1);
         }
         
-        _lineStarts = lineStarts.ToArray();
+        _lines = lines.ToArray();
     }
     
     public TextPosition GetPosition(int position)
     {
-        var lineIndex = Array.FindLastIndex(_lineStarts, lineStart => lineStart <= position);
-        var lineStart = _lineStarts[lineIndex];
-        return new TextPosition(lineIndex + 1, position - lineStart + 1);
+        var lineIndex = GetLineIndex(position);
+        return new TextPosition(lineIndex + 1, position - _lines[lineIndex] + 1);
+    }
+
+    private int GetLineIndex(int position)
+    {
+        var lower = 0;
+        var upper = _lines.Length - 1;
+
+        while (lower <= upper)
+        {
+            var index = lower + (upper - lower) / 2;
+            var lineStart = _lines[index];
+
+            if (lineStart == position)
+                return index;
+            
+            if (position < lineStart)
+                upper = index - 1;
+            else
+                lower = index + 1;
+        }
+
+        return lower - 1;
     }
 }
 
 public sealed record TextSpan(int Start, int Length)
 {
     public int End => Start + Length;
+    
+    public bool Contains(int position) => Start <= position && position < End;
 }
 
 public sealed record TextLocation(TextPosition Start, TextPosition End);
