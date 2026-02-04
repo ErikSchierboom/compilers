e57336627247f842650db361e5e84f57d1c71890
Erik Schierboom <erik_schierboom@hotmail.com>
2026-02-04 10:05:35 +0100

    Add basic binder

--- boomscript/BoomScript/Program.cs
+++ boomscript/BoomScript/Program.cs
@@ -1,6 +1,11 @@
 ﻿using BoomScript;
 
-foreach (var expression in Parser.Parse(new SourceText("x = y = 1 + 2 * 3")))
+var sourceText = new SourceText("x = 1 + 2 * 3\ny = x > 4");
+
+var expressions = Parser.Parse(sourceText);
+foreach (var expression in expressions)
     Console.WriteLine(expression);
 
-Console.WriteLine(Interpreter.Evaluate(new SourceText("x = 1 + 2 * 3\ny = x > 4")));
+Console.WriteLine(Binder.Bind(expressions));
+
+Console.WriteLine(Interpreter.Evaluate(sourceText));
