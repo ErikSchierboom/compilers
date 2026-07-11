09499ff77d5541de5cd7629f257a37b9ebc30aa0
Erik Schierboom <erik_schierboom@hotmail.com>
2026-07-11 19:22:12 +0200

    Simplify boolean expression

--- gleamy/Gleamy/Parser.cs
+++ gleamy/Gleamy/Parser.cs
@@ -215,7 +215,7 @@ internal class Parser
     
         var cases = new List<MatchCase>();
     
-        while (!(Current.Type == TokenType.CloseBracket))
+        while (Current.Type != TokenType.CloseBracket)
         {
             do
             {
@@ -276,7 +276,7 @@ internal class Parser
     private Expression ParseCall(Expression left)
     {
         var arguments = new List<Expression>();
-        while (!(Current.Type == TokenType.CloseParen))
+        while (Current.Type != TokenType.CloseParen)
         {
             do
             {
