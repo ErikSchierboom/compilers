21c86cb126aedd6e6f7cf297a57a1cfe31759383
Erik Schierboom <erik_schierboom@hotmail.com>
2026-07-11 21:59:30 +0200

    Add unary tests

--- gleamy/Gleamy.Tests/ArithmeticTests.cs
+++ gleamy/Gleamy.Tests/ArithmeticTests.cs
@@ -3,11 +3,45 @@
 public sealed class ArithmeticTests
 {
     [Theory]
+    [InlineData("-1", -1)]
+    [InlineData("-8", -8)]
+    [InlineData("--13", 13)]
+    public void UnaryMinus(string code, int expected) =>
+        Assert.Equal(expected, Interpreter.Evaluate(code));
+    
+    [Theory]
+    [InlineData("+1", 1)]
+    [InlineData("+8", 8)]
+    [InlineData("++13", 13)]
+    public void UnaryPlus(string code, int expected) =>
+        Assert.Equal(expected, Interpreter.Evaluate(code));
+    
+    [Theory]
     [InlineData("1 + 1", 2)]
+    [InlineData("2 + 3", 5)]
+    [InlineData("88 + 0", 88)]
+    public void Addition(string code, int expected) =>
+        Assert.Equal(expected, Interpreter.Evaluate(code));
+    
+    [Theory]
+    [InlineData("1 - 1", 0)]
+    [InlineData("5 - 3", 2)]
+    [InlineData("88 - 0", 88)]
+    public void Subtraction(string code, int expected) =>
+        Assert.Equal(expected, Interpreter.Evaluate(code));
+    
+    [Theory]
     [InlineData("2 * 3", 6)]
-    [InlineData("4 - 2", 2)]
-    [InlineData("10 / 2", 5)]
-    public void Simple(string code, int expected) =>
+    [InlineData("4 * 0", 0)]
+    [InlineData("11 * 1", 11)]
+    public void Multiplication(string code, int expected) =>
+        Assert.Equal(expected, Interpreter.Evaluate(code));
+    
+    [Theory]
+    [InlineData("6 / 3", 2)]
+    [InlineData("15 / 3", 5)]
+    [InlineData("10 / 1", 10)]
+    public void Division(string code, int expected) =>
         Assert.Equal(expected, Interpreter.Evaluate(code));
     
     [Theory]
