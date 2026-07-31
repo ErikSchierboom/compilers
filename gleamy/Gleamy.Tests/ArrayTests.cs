c45e2e6637764c298bc3653f9438c0353d03aa89
Erik Schierboom <erik_schierboom@hotmail.com>
2026-07-31 13:17:57 +0200

    Reorder tests

--- gleamy/Gleamy.Tests/ArrayTests.cs
+++ gleamy/Gleamy.Tests/ArrayTests.cs
@@ -1,20 +1,20 @@
 namespace Gleamy.Tests;
 
 public class ArrayTests
-{
-    [Theory]
-    [InlineData("[1]", new object[] { 1 })]
-    [InlineData("[true, false]", new object[] { true, false })]
-    [InlineData("[13, 15, 17]", new object[] { 13, 15, 17 })]
-    public void NonEmpty(string code, object[] expected) =>
-        Assert.Equal(expected, Interpreter.Evaluate(code));
-    
+{   
     [Fact]
     public void Empty()
     {
         const string source = "[]";
         Assert.Equal(Array.Empty<object>(), Interpreter.Evaluate(source));
     }
+
+    [Theory]
+    [InlineData("[1]", new object[] { 1 })]
+    [InlineData("[true, false]", new object[] { true, false })]
+    [InlineData("[13, 15, 17]", new object[] { 13, 15, 17 })]
+    public void NonEmpty(string code, object[] expected) =>
+        Assert.Equal(expected, Interpreter.Evaluate(code));
 
     [Theory]
     [InlineData("[[1], [2, 3]]", new object[] { new object[] { 1 }, new object[] { 2, 3 } })]
