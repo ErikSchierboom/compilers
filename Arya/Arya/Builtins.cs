cd69db86ae128c30f56e5fd934695b7f8ed15347
Erik Schierboom <erik_schierboom@hotmail.com>
2026-08-06 13:05:34 +0200

    Don't expose internal functions

--- Arya/Arya/Builtins.cs
+++ Arya/Arya/Builtins.cs
@@ -1,43 +1,47 @@
 namespace Arya;
 
-public static class BuiltinFunctions
+internal static class BuiltinFunctions
 {
     public static readonly Function[] All =
     [
+        // Unary integer functions
         new AbsFunction(),
+        
+        // Unary string functions
         new LowercaseFunction(),
         new UppercaseFunction(),
         new TrimFunction()
     ];
-}
 
-public abstract record UnaryIntegerFunction(string Name, Func<int, int> Operation) : Function(Name)
-{
-    public override int Arity => 1;
+    private abstract record UnaryIntegerFunction(string Name, Func<int, int> Operation) : Function(Name)
+    {
+        public override int Arity => 1;
     
-    public override Value Invoke(params Value[] arguments) =>
-        arguments[0] switch
-        {
-            Array array => Array.UnaryOp(array, Operation),
-            Integer integer => new Integer(Operation(integer.Value)),
-            _ => throw new InvalidOperationException("Invalid argument type")
-        };
-}
-
-public abstract record UnaryStringFunction(string Name, Func<string, string> Operation) : Function(Name)
-{
-    public override int Arity => 1;
+        public override Value Invoke(params Value[] arguments) =>
+            arguments[0] switch
+            {
+                Array array => Array.UnaryOp(array, Operation),
+                Integer integer => new Integer(Operation(integer.Value)),
+                _ => throw new InvalidOperationException("Invalid argument type")
+            };
+    }
     
-    public override Value Invoke(params Value[] arguments) =>
-        arguments[0] switch
-        {
-            Array array => Array.UnaryOp(array, Operation),
-            String str => new String(Operation(str.Value)),
-            _ => throw new InvalidOperationException("Invalid argument type")
-        };
-}
+    private sealed record AbsFunction() : UnaryIntegerFunction("abs", Math.Abs);
 
-public sealed record AbsFunction() : UnaryIntegerFunction("abs", Math.Abs);
-public sealed record LowercaseFunction() : UnaryStringFunction("lowercase", str => str.ToLowerInvariant());
-public sealed record UppercaseFunction() : UnaryStringFunction("uppercase", str => str.ToUpperInvariant());
-public sealed record TrimFunction() : UnaryStringFunction("trim", str => str.Trim());
+    private abstract record UnaryStringFunction(string Name, Func<string, string> Operation) : Function(Name)
+    {
+        public override int Arity => 1;
+    
+        public override Value Invoke(params Value[] arguments) =>
+            arguments[0] switch
+            {
+                Array array => Array.UnaryOp(array, Operation),
+                String str => new String(Operation(str.Value)),
+                _ => throw new InvalidOperationException("Invalid argument type")
+            };
+    }
+
+    private sealed record LowercaseFunction() : UnaryStringFunction("lowercase", str => str.ToLowerInvariant());
+    private sealed record UppercaseFunction() : UnaryStringFunction("uppercase", str => str.ToUpperInvariant());
+    private sealed record TrimFunction() : UnaryStringFunction("trim", str => str.Trim());
+}
