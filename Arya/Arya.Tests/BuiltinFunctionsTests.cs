2f96559809cfe3914669cdd3c875187d9a65949f
Erik Schierboom <erik_schierboom@hotmail.com>
2026-08-06 08:06:02 +0200

    Add test cases for abs with nested arrays

--- Arya/Arya.Tests/BuiltinFunctionsTests.cs
+++ Arya/Arya.Tests/BuiltinFunctionsTests.cs
@@ -9,7 +9,7 @@ public class BuiltinFunctionsTests
             {
                 { "abs(-1)", new Integer(1) },
                 { "abs([-1 -2 -3])", new Array(new Integer(1), new Integer(2), new Integer(3)) },
-                // { "abs([[4 5] [6 7]])", new Array(new Array(new Integer(4), new Integer(5)), new Array(new Integer(6), new Integer(7))) }
+                { "abs([[-4 -5] [-6 -7]])", new Array(new Array(new Integer(4), new Integer(5)), new Array(new Integer(6), new Integer(7))) }
             };
 
         [Theory, MemberData(nameof(IntegersTestData))]
