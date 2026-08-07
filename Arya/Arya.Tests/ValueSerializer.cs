9dabc567bf10038aae92c75792efd603930dc848
Erik Schierboom <erik_schierboom@hotmail.com>
2026-08-07 09:08:18 +0200

    Add Value serializer

---
+++ Arya/Arya.Tests/ValueSerializer.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Text.Json;
+using Arya;
+using Arya.Tests;
+using Xunit.Sdk;
+
+[assembly: RegisterXunitSerializer(typeof(ValueSerializer), typeof(Value))]
+
+namespace Arya.Tests;
+
+public class ValueSerializer : IXunitSerializer
+{
+    public bool IsSerializable(Type type, object? value, [NotNullWhen(false)] out string? failureReason)
+    {
+        if (type != typeof(Value))
+        {
+            failureReason = $"Can only serialize {nameof(Value)} instances";
+            return false;
+        }
+
+        failureReason = null;
+        return true;
+    }
+    
+    public object Deserialize(Type type, string serializedValue) => JsonSerializer.Deserialize<Value>(serializedValue)!;
+
+    public string Serialize(object value) => JsonSerializer.Serialize(value);
+}
